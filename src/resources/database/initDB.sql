80ec4ad573fac4f7928d8ef6fa04da32f50545df
melnyknikolay <nikmlk87@gmail.com>
2016-06-10 07:14:04 +0300

    master

--- src/resources/database/initDB.sql
+++ src/resources/database/initDB.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS company;
+CREATE SEQUENCE global_seq START 1;
 CREATE TABLE company
 (
-  id integer NOT NULL,
+  id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   company_name character varying NOT NULL,
   earnings character varying NOT NULL,
-  parrent_id integer NOT NULL DEFAULT 0,
-  CONSTRAINT company_pkey PRIMARY KEY (id)
+  parrent_id integer NOT NULL DEFAULT 0
 )
